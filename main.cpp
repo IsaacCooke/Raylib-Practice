01371cc8ba4189707b269db48b18d029e1746f17
Isaac Cooke <cookeisaac2@gmail.com>
2024-02-21 22:05:11 +0000

    Added movement system (badly)

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <raylib.h>
+#include <iostream>
 
 int main(){
   const int screenHeight = 400;
@@ -6,13 +7,25 @@ int main(){
 
   InitWindow(screenWidth, screenHeight, "Raylib Test");
 
+  Vector2 ballPosition = {(float)screenWidth/2, (float){screenHeight}/2};
+
   SetTargetFPS(60);
 
   while(!WindowShouldClose()){
+    if(IsKeyDown(KEY_RIGHT || KEY_D)) ballPosition.x += 2.0f;
+    if(IsKeyDown(KEY_LEFT || KEY_A)) ballPosition.x -= 2.0f;
+    if(IsKeyDown(KEY_DOWN || KEY_S)) ballPosition.y += 2.0f;
+    if(IsKeyDown(KEY_UP || KEY_W)) ballPosition.y -= 2.0f;
+
     BeginDrawing();
+
     ClearBackground(WHITE);
-    DrawText("Window Created", 190, 200, 20, GREEN);
+
+    DrawCircleV(ballPosition, 50, RED);
+
     EndDrawing();
+
+    std::cout << "Frame Running";
   }
 
   CloseWindow();
