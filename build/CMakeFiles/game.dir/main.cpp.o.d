e994a4e8093a7ed3d5c39edb4fbd68907fe060b4
Isaac Cooke <cookeisaac2@gmail.com>
2024-02-22 17:39:08 +0000

    Swapped to CMake for build system

---
+++ build/CMakeFiles/game.dir/main.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/game.dir/main.cpp.o: /home/isaac/Documents/ray/main.cpp \
+ /usr/include/stdc-predef.h /usr/include/raylib.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/13.2.1/include/stdarg.h \
+ /home/isaac/Documents/ray/src/player.h
---
+++ build/CMakeFiles/game.dir/src/player.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/game.dir/src/player.cpp.o: \
+ /home/isaac/Documents/ray/src/player.cpp /usr/include/stdc-predef.h \
+ /home/isaac/Documents/ray/src/player.h /usr/include/raylib.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/13.2.1/include/stdarg.h
